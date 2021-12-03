d32b6a950242706c74a81c8faaed41acb7a56e13
Divyajeet chaudhray <95498980+divyajeet-Chaudhary@users.noreply.github.com>
2021-12-04 00:33:13 +0545

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int
+main ()
+{
+  int a = 100, b = 10;
+  int sum = a + b;
+  int sub = a - b;
+  int mul = a * b;
+  int divd = a / b;
+  int mod = a % b;
+  printf ("the sum is %d\n",sum);
+  printf("the sub is %d\n",sub);
+  printf ("the mul is %d\n",mul);
+  printf("the divd is %d\n",divd);
+  printf("the mod is %d\n",mod);
+  return 0;
+}
