b97ab55f98e176a8c632091094573e9751be99ff
Divyajeet chaudhray <95498980+divyajeet-Chaudhary@users.noreply.github.com>
2021-12-04 14:37:40 +0545

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<stdbool.h>
+int main()
+{
+    bool a =true;
+    printf("%d",a);
+    return 0;
+    //output will be 1
+    //if we use bool a=false; in 5 line 
+    //output will be 0
+
+}
