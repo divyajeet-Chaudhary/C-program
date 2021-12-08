9258fbfeb64259a38aca28cd241d6866ee8bff83
Divyajeet chaudhray <95498980+divyajeet-Chaudhary@users.noreply.github.com>
2021-12-08 23:16:11 +0545

    Create 8.c

---
+++ 8.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float a,sum;
+    int b;
+    printf("a= ");
+    scanf("%f",&a);
+    printf("b= ");
+    scanf("%d",&b);
+    sum=(a+b);
+    printf("sum of a+b =%.1lf",sum);
+    return 0;
+}
